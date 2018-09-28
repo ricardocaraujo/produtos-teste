76ff40bbabec8cbc64bce179b9aaf47fe0c4e46b
behx <behx@queimados.ep.petrobras.com.br>
2018-09-28 18:47:26 +0000

    Primeiro commit

---
+++ produtos/src/main/java/br/com/testes/loja/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package br.com.testes.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/teste")
+	public String olaMundo() {
+		return "ok";
+	}
+
+}
---
+++ produtos/src/main/java/br/com/testes/loja/resources/ProdutoResource.java
@@ -0,0 +1,18 @@
+package br.com.testes.loja.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("produtos")
+public class ProdutoResource {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public String busca() {
+		return "<pasta de dente>colgate</pasta de dente>";
+	}
+	
+	
+}
